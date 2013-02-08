86054180510270854ad65f488a4facc3d0ae1738
Mike Frey <frey.mike@gmail.com>
2013-02-08 15:20:37 -0600

    Removed dependencies.
    Removed lodash and added underscore's extend function to the file to remove all external dependencies.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict'
-var _ = require('lodash');
 
 /**
  * Backbone.js helper functions, these have been copy-pasted from the backbone.js
@@ -11,7 +10,7 @@ var _ = require('lodash');
 // Helper function to correctly set up the prototype chain, for subclasses.
 // Similar to `goog.inherits`, but uses a hash of prototype properties and
 // class properties to be extended.
-module.exports = function extend(protoProps, staticProps) {
+module.exports = function(protoProps, staticProps) {
   var parent = this;
   var child;
 
@@ -21,25 +20,36 @@ module.exports = function extend(protoProps, staticProps) {
   if (protoProps && _.has(protoProps, 'constructor')) {
     child = protoProps.constructor;
   } else {
-    child = function(){ parent.apply(this, arguments); };
+    child = function(){ return parent.apply(this, arguments); };
   }
+
+  // Add static properties to the constructor function, if supplied.
+  extend(child, parent, staticProps);
 
   // Set the prototype chain to inherit from `parent`, without calling
   // `parent`'s constructor function.
-  function Surrogate(){ this.constructor = child; };
+  var Surrogate = function(){ this.constructor = child; };
   Surrogate.prototype = parent.prototype;
   child.prototype = new Surrogate;
 
   // Add prototype properties (instance properties) to the subclass,
   // if supplied.
-  if (protoProps) _.extend(child.prototype, protoProps);
-
-  // Add static properties to the constructor function, if supplied.
-  _.extend(child, parent, staticProps);
+  if (protoProps) extend(child.prototype, protoProps);
 
   // Set a convenience property in case the parent's prototype is needed
   // later.
   child.__super__ = parent.prototype;
 
   return child;
+};
+
+var extend = function(obj) {
+  Array.prototype.slice.call(arguments, 1).forEach(function(source) {
+    if (source) {
+      for (var prop in source) {
+        obj[prop] = source[prop];
+      }
+    }
+  });
+  return obj;
 };
